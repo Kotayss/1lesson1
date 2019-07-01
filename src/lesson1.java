b18e518384b3362859a50ea1b9f78811933aeb51
Andrew <kotayss30@gmail.com>
2019-07-01 22:27:55 +0300

    Update lesson1.java

--- src/lesson1.java
+++ src/lesson1.java
@@ -10,8 +10,8 @@ public class lesson1 {
         String s = reader.readLine();
         System.out.println("введите b: ");
         String s1 = reader.readLine();
-        int a = Integer.parseInt(s);
-        int b = Integer.parseInt(s1);
+        float a = Float.parseFloat(s);
+        float b = Float.parseFloat(s1);
         if(a != 0)
             System.out.println("x = -b/a\n" + "x = " + (-b)/a);
         else
